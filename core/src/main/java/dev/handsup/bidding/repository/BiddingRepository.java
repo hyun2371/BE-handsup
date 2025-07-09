abe08c3489c596c979ebad0db7a4aa10bc0d39fd
hs12 <loveanna1228@gmail.com>
2025-07-09 16:23:12 +0900

    [test] : 락 테스트 추가

--- core/src/main/java/dev/handsup/bidding/repository/BiddingRepository.java
+++ core/src/main/java/dev/handsup/bidding/repository/BiddingRepository.java
@@ -38,4 +38,5 @@ public interface BiddingRepository extends JpaRepository<Bidding, Long> {
 	Slice<Bidding> findByBidderAndAuction_StatusOrderByAuction_CreatedAtDesc(
 		User bidder, AuctionStatus auctionStatus, Pageable pageable);
 
+	Long countByAuctionId(Long auctionId);
 }
--- core/src/test/java/dev/handsup/bidding/service/AuctionWithLockTest.java
+++ core/src/test/java/dev/handsup/bidding/service/AuctionWithLockTest.java
@@ -1,11 +1,17 @@
 package dev.handsup.bidding.service;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,9 +38,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Import({TestAuditingConfig.class, FcmConfig.class})
 public class AuctionWithLockTest extends TestContainerSupport {
+
 	private Auction auction;
 	private User user;
-	private RegisterBiddingRequest request;
 	private int threadCount, poolSize;
 
 	@Autowired
@@ -52,29 +58,39 @@ public class AuctionWithLockTest extends TestContainerSupport {
 	@Autowired
 	private BiddingRepository biddingRepository;
 
+	@Autowired
+	private OptimisticLockAuctionFacade optimisticLockAuctionFacade;
+
 	@BeforeEach
 	void setUp() {
 		ProductCategory productCategory = productCategoryRepository.save(ProductCategory.from("디지털 기기"));
 		auction = auctionRepository.save(AuctionFixture.auction(productCategory));
-		request = RegisterBiddingRequest.from(auction.getInitPrice() + 1000);
-
 		user = userRepository.save(UserFixture.user1());
-		threadCount = 100;
+		threadCount = 2000;
 		poolSize = 32;
 	}
 
+	@AfterEach
+	void clean(){
+		biddingRepository.deleteAll();
+	}
+
+	@Disabled
 	@DisplayName("[동시 요청 시, 입찰 금액이 모두 같다면 하나의 입찰만 저장된다.]")
 	@Test
-	void distributed_lock_test() throws InterruptedException {
+	void distributed_lock_same_bidding_price() throws InterruptedException {
+		RegisterBiddingRequest request = RegisterBiddingRequest.from(auction.getInitPrice() + 1000);
 		ExecutorService executorService = Executors.newFixedThreadPool(poolSize);
 		CountDownLatch latch = new CountDownLatch(threadCount);
 
-		StopWatch stopWatch = new StopWatch();
+		StopWatch stopWatch = new StopWatch(
+			String.format("경매 분산락 기반 입찰 중복 방지 테스트\n(스레드 %d, 스레드 풀 %d개)", threadCount, poolSize)
+		);
 		stopWatch.start();
 		for (int i = 0; i < threadCount; i++) {
 			executorService.submit(() -> {
 				try {
-					biddingService.registerBidding(request, auction.getId(), user);
+					biddingService.registerBiddingWithDistributedLock(request, auction.getId(), user);
 				} catch (Exception e) {
 					log.info("{concurrency test error = {}", e.getMessage());
 				} finally {
@@ -86,23 +102,26 @@ public class AuctionWithLockTest extends TestContainerSupport {
 		executorService.shutdown();
 
 		stopWatch.stop();
-		log.info("동시성 테스트 소요 시간(ms): {}", stopWatch.getTotalTimeMillis());
+		System.out.println(stopWatch.prettyPrint());
 
 		Assertions.assertThat(biddingRepository.findAll()).hasSize(1);
 	}
 
 	@DisplayName("[동시 요청 시, 입찰 금액이 모두 같다면 하나의 입찰만 저장된다.]")
 	@Test
-	void concurrency_test() throws InterruptedException {
+	void pessimistic_lock_same_bidding_price() throws InterruptedException {
+		RegisterBiddingRequest request = RegisterBiddingRequest.from(auction.getInitPrice() + 1000);
 		ExecutorService executorService = Executors.newFixedThreadPool(poolSize);
 		CountDownLatch latch = new CountDownLatch(threadCount);
 
-		StopWatch stopWatch = new StopWatch();
+		StopWatch stopWatch = new StopWatch(
+			String.format("경매 비관락 기반 입찰 중복 방지 테스트\n(스레드 %d, 스레드 풀 %d개)", threadCount, poolSize)
+		);
 		stopWatch.start();
 		for (int i = 0; i < threadCount; i++) {
 			executorService.submit(() -> {
 				try {
-					biddingService.registerBidding(request, auction.getId(), user);
+					biddingService.registerBiddingWithPessimisticLock(request, auction.getId(), user);
 				} catch (Exception e) {
 					log.info("{concurrency test error = {}", e.getMessage());
 				} finally {
@@ -114,8 +133,128 @@ public class AuctionWithLockTest extends TestContainerSupport {
 		executorService.shutdown();
 
 		stopWatch.stop();
-		log.info("동시성 테스트 소요 시간(ms): {}", stopWatch.getTotalTimeMillis());
+		System.out.println(stopWatch.prettyPrint());
 
 		Assertions.assertThat(biddingRepository.findAll()).hasSize(1);
+	}
+
+	@DisplayName("[동시 요청 시, 입찰 금액이 모두 같다면 하나의 입찰만 저장된다.]")
+	@Test
+	void optimistic_lock_same_bidding_price() throws InterruptedException {
+		RegisterBiddingRequest request = RegisterBiddingRequest.from(auction.getInitPrice() + 1000);
+		ExecutorService executorService = Executors.newFixedThreadPool(poolSize);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		StopWatch stopWatch = new StopWatch(
+			String.format("경매 낙관락 기반 입찰 중복 방지 테스트\n(스레드 %d, 스레드 풀 %d개)", threadCount, poolSize)
+		);
+		stopWatch.start();
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					optimisticLockAuctionFacade.registerBidding(request, auction.getId(), user);
+				} catch (Exception e) {
+					log.info("{concurrency test error = {}", e.getMessage());
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+		executorService.shutdown();
+
+		stopWatch.stop();
+		System.out.println(stopWatch.prettyPrint());
+
+		Assertions.assertThat(biddingRepository.findAll()).hasSize(1);
+	}
+
+	@DisplayName("[여러 금액 동시 입찰 시, 최고가 갱신 입찰만 저장된다.]")
+	@Test
+	void optimistic_lock_diff_bidding_price() throws InterruptedException {
+		ExecutorService executor = Executors.newFixedThreadPool(poolSize);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		AtomicInteger successCount = new AtomicInteger();
+		AtomicInteger failCount = new AtomicInteger();
+
+		Random random = new Random();
+		StopWatch stopWatch = new StopWatch("Concurrent Optimistic Bidding Test");
+		stopWatch.start();
+
+		for (int i = 0; i < threadCount; i++) {
+			int rand = 1 + random.nextInt(5); // 대략 20% 정도 입찰 금액 겹치도록 설정
+			final int biddingPrice = auction.getInitPrice() + rand * 1000;
+			executor.submit(() -> {
+				try {
+					optimisticLockAuctionFacade.registerBidding(
+						RegisterBiddingRequest.from(biddingPrice),
+						auction.getId(),
+						user
+					);
+					successCount.incrementAndGet();
+				} catch (Exception e) {
+					log.info("{concurrency test error = {}", e.getMessage());
+					failCount.incrementAndGet();
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+		executor.shutdown();
+
+		stopWatch.stop();
+		System.out.println(stopWatch.prettyPrint());
+
+		assertThat(biddingRepository.countByAuctionId(auction.getId()))
+			.isEqualTo(successCount.get());
+		assertThat(successCount.get() + failCount.get()).isEqualTo(threadCount);
+	}
+
+	@DisplayName("[여러 금액 동시 입찰 시, 최고가 갱신 입찰만 저장된다.]")
+	@Test
+	void pessimistic_lock_diff_bidding_price() throws InterruptedException {
+		ExecutorService executor = Executors.newFixedThreadPool(poolSize);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		AtomicInteger successCount = new AtomicInteger();
+		AtomicInteger failCount = new AtomicInteger();
+
+		Random random = new Random();
+
+		StopWatch stopWatch = new StopWatch("Concurrent Pessimistic Bidding Test");
+		stopWatch.start();
+
+		for (int i = 0; i < threadCount; i++) {
+			int rand = 1 + random.nextInt(5); // 대략 20% 정도 입찰 금액 겹치도록 설정
+			final int biddingPrice = auction.getInitPrice() + rand * 1000;
+			executor.submit(() -> {
+				try {
+					biddingService.registerBiddingWithPessimisticLock(
+						RegisterBiddingRequest.from(biddingPrice),
+						auction.getId(),
+						user
+					);
+					successCount.incrementAndGet();
+				} catch (Exception e) {
+					log.info("{concurrency test error = {}", e.getMessage());
+					failCount.incrementAndGet();
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+		executor.shutdown();
+
+		stopWatch.stop();
+		System.out.println(stopWatch.prettyPrint());
+
+		assertThat(biddingRepository.countByAuctionId(auction.getId()))
+			.isEqualTo(successCount.get());
+		assertThat(successCount.get() + failCount.get()).isEqualTo(threadCount);
 	}
 }
